8107a361cedf213a2e88f2807393362fd83413b0
queuing4oranges <katjazenker@gmail.com>
2024-10-06 15:28:06 +0200

    Minor fixes on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,14 +10,14 @@ export default function Navbar() {
 			<KvirtivityLogo height={10} width={51}/>
 		</Col>
 		<Col>
-			<span className='d-flex justify-content-end'>
-				<a className='me-3' href='mailto:kvirtivity@gmail.com' target='_blank' rel='noreferrer'>
+			<span className='d-flex justify-content-end align-items-center'>
+				<a className='me-3' title='Contact us!'  href='mailto:kvirtivity@gmail.com' target='_blank' rel='noreferrer'>
 					<i className='bi bi-envelope' />
 				</a>
-				<a className='me-3' href='https://www.instagram.com/kvirtivity'target='_blank' rel='noreferrer'>
+				<a className='me-3'title='Instagram' href='https://www.instagram.com/kvirtivity'target='_blank' rel='noreferrer'>
 					<i className='bi bi-instagram' />
 				</a>
-				<a href='https://www.facebook.com/profile.php?id=61564294108931'target='_blank' rel='noreferrer'>
+				<a title='Facebook' href='https://www.facebook.com/profile.php?id=61564294108931'target='_blank' rel='noreferrer'>
 					<i className='bi bi-facebook' />
 				</a>
 			</span>
